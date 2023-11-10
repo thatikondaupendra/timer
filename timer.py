77cbc4018e25cf11050fc66ec07c8dc9318222af
thatikondaupendra <140628435+thatikondaupendra@users.noreply.github.com>
2023-11-10 06:39:58 +0530

    Update timer.py

--- timer.py
+++ timer.py
@@ -3,6 +3,6 @@ n=int(input("enter time in seconds: "))
 while n:
     min=n//60
     sec=n%60
-    print("{:2d}:{:2d}".format(min,sec),end="\r")
+    print("{:02d}:{:02d}".format(min,sec),end="\r")
     time.sleep(1)
     n-=1
